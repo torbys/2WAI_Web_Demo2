4a7d9e99dca47ffc25071dafb4af72b4eda2421b
DDsheng <ddsheng_new@163.com>
2025-01-03 19:02:54 +0800

    1.3日19.02提交

--- client_wn.js
+++ client_wn.js
@@ -19,6 +19,8 @@ function negotiate() {
                 };
                 pc.addEventListener('icegatheringstatechange', checkState);
             }
+
+          
         });
     }).then(() => {
         var offer = pc.localDescription; 
@@ -35,17 +37,25 @@ function negotiate() {
             },
             method: 'POST'
         });
+
+        
     }).then((response) => {
         return response.json();
     }).then((answer) => {
         sessionid = answer.sessionid
+        
         return pc.setRemoteDescription(answer);
+
     }).catch((e) => {
         alert(e);
     });
+
+
+    
 }
 
 function start() {
+
     var config = {
         sdpSemantics: 'unified-plan'
     };
@@ -58,7 +68,7 @@ function start() {
         config.iceServers = [{ urls: ['stun:stun.l.google.com:19302'] }];
     }
 
-    pc = new RTCPeerConnection(config);
+    pc = new RTCPeerConnection(config);    
 
     // connect audio / video
     pc.addEventListener('track', (evt) => {
@@ -67,15 +77,45 @@ function start() {
 			var videoElement = document.getElementById('video');
 			        videoElement.srcObject = evt.streams[0];
 			        videoElement.loop = false; 
-            document.getElementById('video').srcObject = evt.streams[0];
+            // document.getElementById('video').srcObject = evt.streams[0];
         } else {
             document.getElementById('audio').srcObject = evt.streams[0];
         }
     });
 
+    
     document.getElementById('start').style.display = 'none';
     negotiate();
     document.getElementById('stop').style.display = 'inline-block';
+
+    // 这个函数用于定期检查 pc 的状态
+    function checkConnectionState() {
+        if (!isConnected) {
+            console.log("连接有误继续loading");
+            showLoading(); // 显示 loading 提示
+        } else {
+            console.log("连接成功取消loading");
+            hideLoading(); // 隐藏 loading 提示
+        }
+    }
+
+    console.log("初始化检查链接");
+    checkConnectionState()
+    
+    // 设置定时器，每2000毫秒（2秒）检查一次状态
+    const intervalId = setInterval(checkConnectionState, 2000);
+    const timeoutElement = document.querySelector('.Time-out');
+
+    // 设置一个定时器，在40秒后停止重复检查
+    setTimeout(() => {
+        console.log("20秒后停止检查连接状态");
+        clearInterval(intervalId); // 停止定时器
+        hideLoading(); // 隐藏 loading 提示
+        if (!isConnected)(
+            timeoutElement.classList.add('pop') // 显示连接超时提示
+        )
+    }, 40000); // 40秒
+
 }
 
 function stop() {
@@ -87,6 +127,9 @@ function stop() {
     setTimeout(() => {
         pc.close();
     }, 500);
+
+   
+
     console.log("********************888***********************************");
 }
 
@@ -97,6 +140,13 @@ function stop2() {
         pc.close();
         pc = null; 
     }
+
+    var textElement = document.querySelector('.Error-Dialog .text');
+    if (textElement) {
+        textElement.textContent = "Changing, please wait...";
+    }
+
+    showLoading();
 
     // 如果你还需要清理音频/视频流等资源，可以在这里添加更多清理代码
     // document.getElementById('video').srcObject = null;
